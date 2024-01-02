7489e26475282fcf1fb09c7872e4885d46756d68
CiumbaSpe <filippo.speziali01@gmail.com>
2024-01-02 21:22:49 +0100

    update

--- train.py
+++ train.py
@@ -19,7 +19,7 @@ DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 BATCH_SIZE = 4
 NUM_EPOCHS = 20
 NUM_WORKERS = 1
-TRAIN_DIR_X = 'undersample_dataset/120_trainIn'
+TRAIN_DIR_X = 'undersample_dataset/240_trainIn'
 TRAIN_DIR_Y = 'undersample_dataset/trainOut'
 # VAL_DIR_X = 'new_mayo/FBPB/mayo_val/'
 # VAL_DIR_Y = 'new_mayo/GT/mayo_val/' 
@@ -88,7 +88,7 @@ def main():
         "state_dict": model.state_dict(),
         "optimizer": optimizer.state_dict(),
     }
-    save_checkpoint(checkpoint, "120_first_train.pth.tar")
+    save_checkpoint(checkpoint, "240_first_train.pth.tar")
 
 if __name__ == "__main__":
     main()
