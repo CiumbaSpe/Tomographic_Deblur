e3ca468b40d4e2d67516a39896d6cdd4077d4ef0
filippo.speziali <filippo.speziali@studio.unibo.it>
2024-02-25 17:48:55 +0100

    update

--- test/test.py
+++ test/test.py
@@ -5,6 +5,7 @@ import sys
 from sklearn.metrics import mean_squared_error
 from skimage.metrics import structural_similarity as ssim
 from skimage.metrics import peak_signal_noise_ratio as psnr
+import tqdm
 
 sys.path.insert(0, '../')
 sys.path.insert(0, '../3d')
@@ -16,7 +17,7 @@ from better_model import ResUnet2d
 from better_model import FullResUnet2d
 # from model import UNET_3d
 from utils.utils import (
-    load_checkpoint
+    load_checkpoint, get_loaders
 )
 
 sys.path.insert(0, '../')
@@ -27,6 +28,9 @@ sys.path.insert(0, '../2d')
 CHECKPOINT = "weights/first_seetrough.pth.tar" # default value
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 MODEL = ResUnet2d(in_channels=1, out_channels=1).to(DEVICE) 
+BATCH_SIZE = 4
+DIMENSION = '2d'
+
 # cosa vuoi testare
 GTDIR = "../SeeTrough/gigadose/JTS/testOut/"
 NOISEDIR = "../SeeTrough/gigadose/JTS/testIn/"
@@ -34,14 +38,21 @@ NOISEDIR = "../SeeTrough/gigadose/JTS/testIn/"
 #GTDIR = "../SeeTrough/undersample/JTS/testOut/"
 #NOISEDIR = "../SeeTrough/undersample/JTS/240_testIn/"
 
-def pred_image(image, model):
+# def pred_image(image, model):
+#         model.eval()
+#         with torch.no_grad(): # does not calculate gradient
+#             preds = torch.from_numpy(image).unsqueeze(0).unsqueeze(0).to(DEVICE)
+#             preds_tensor = model(preds)
+
+#             preds_tensor = preds_tensor.squeeze(0).squeeze(0).cpu()
+#             return preds_tensor.numpy()
+
+def pred_image(data, model = MODEL):
         model.eval()
         with torch.no_grad(): # does not calculate gradient
-            preds = torch.from_numpy(image).unsqueeze(0).unsqueeze(0).to(DEVICE)
-            preds_tensor = model(preds)
-
+            preds_tensor = model(data)
             preds_tensor = preds_tensor.squeeze(0).squeeze(0).cpu()
-            return preds_tensor.numpy()
+            return preds_tensor.numpy() 
 
 
 def test(model):
@@ -53,34 +64,55 @@ def test(model):
     ssim_corrupted = 0
     ssim_pred = 0
 
-    n = len(os.listdir(GTDIR))
-    
+    loader = get_loaders(
+        DIMENSION,
+        NOISEDIR,
+        GTDIR,
+        BATCH_SIZE,
+    )
+
+    loop = tqdm(loader) 
+
     cont = 0
-    for i in sorted(os.listdir(GTDIR)):
-        if (cont >= 21 or cont <= 839):
-            gt = np.load(GTDIR + i)
-            noise = np.load(NOISEDIR + i)
+    loss_sum = 0
+    # RUNNING TROUGH ALL THE BATCHES
+    MODEL.eval()
+    for idx, (x, y) in enumerate(loop):
+        x = torch.unsqueeze(x, 1).to(device = DEVICE)    
+        preds = pred_image(x, model)
+        cont += 1
+        loss_sum += mean_squared_error(y, preds)
+
+    # n = len(os.listdir(GTDIR))
+    
+    # cont = 0
+    # for i in sorted(os.listdir(GTDIR)):
+    #     if (cont >= 21 or cont <= 839):
+    #         gt = np.load(GTDIR + i)
+    #         noise = np.load(NOISEDIR + i)
         
-            mse_corrupted += mean_squared_error(gt, noise)
-            psnr_corrupted += psnr(gt, noise)
-            ssim_corrupted += ssim(gt, noise, data_range=noise.max() - noise.min())
+    #         mse_corrupted += mean_squared_error(gt, noise)
+    #         psnr_corrupted += psnr(gt, noise)
+    #         ssim_corrupted += ssim(gt, noise, data_range=noise.max() - noise.min())
 
-            preds = pred_image(noise, model)
+    #         preds = pred_image(noise, model)
 
-            mse_pred += mean_squared_error(gt, preds)
-            psnr_pred += psnr(gt, preds)
-            ssim_pred += ssim(gt, preds, data_range=preds.max() - preds.min())
-        cont = cont + 1
+    #         mse_pred += mean_squared_error(gt, preds)
+    #         psnr_pred += psnr(gt, preds)
+    #         ssim_pred += ssim(gt, preds, data_range=preds.max() - preds.min())
+    #     cont = cont + 1
 
 
-    print("average mse for corrupted images in dataset " + NOISEDIR + "\n", mse_corrupted/n)
-    print("average mse for deblurred images\n", mse_pred/n)
+    # print("average mse for corrupted images in dataset " + NOISEDIR + "\n", mse_corrupted/n)
+    # print("average mse for deblurred images\n", mse_pred/n)
 
-    print("\naverage psnr for corrupted images\n", psnr_corrupted/n)
-    print("average psnr for deblurred images\n", psnr_pred/n )
+    # print("\naverage psnr for corrupted images\n", psnr_corrupted/n)
+    # print("average psnr for deblurred images\n", psnr_pred/n )
 
-    print("\naverage ssim for corrupted images\n", ssim_corrupted/n)
-    print("average ssim for deblurred images\n", ssim_pred/n )
+    # print("\naverage ssim for corrupted images\n", ssim_corrupted/n)
+    # print("average ssim for deblurred images\n", ssim_pred/n )
+        
+    print("mse: ", )
 
 
 # load the model and the weights
